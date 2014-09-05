a6d7196c32f4da9ecbbdadcfc3f3895f38ed3a85
Sam Dodrill <xena@yolo-swag.com>
2014-09-05 13:31:14 -0700

    Add support for attaching links to Yo's

--- yo.go
+++ yo.go
@@ -42,12 +42,54 @@ func (c *Client) YoAll() error {
 	return nil
 }
 
+// YoAllLink sends a "Yo" to all subscribed users of the API account
+// with the link specified.
+func (c *Client) YoAllLink(link string) error {
+	data := url.Values{}
+	data.Set("api_token", c.Token)
+	data.Set("link", link)
+
+	res, err := http.PostForm(YO_API+"/yoall/", data)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return errors.New("Received response with non 201 status code.")
+	}
+
+	return nil
+}
+
 // Sends a "Yo" to the specified user (who must subscribe)
 // to the active account. Expects a 201 response.
 func (c *Client) YoUser(username string) error {
 	data := url.Values{}
 	data.Set("api_token", c.Token)
 	data.Set("username", username)
+	res, err := http.PostForm(YO_API+"/yo/", data)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		return errors.New("Received response with non 201 status code.")
+	}
+
+	return nil
+}
+
+// YoUserLink sends a "Yo" to the specified user (who must subscribe) with a link
+// to the active account. Expects a 201 response.
+func (c *Client) YoUserLink(username, link string) error {
+	data := url.Values{}
+	data.Set("api_token", c.Token)
+	data.Set("username", username)
+	data.Set("link", link)
 	res, err := http.PostForm(YO_API+"/yo/", data)
 	if err != nil {
 		return err
